0ef50e51a33c482955c673d46cd75586b7291d9d
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-10-09 16:27:11 +0800

    Create main_numpy.py
    Move main.py to main_numpy.py

---
+++ main_numpy.py
@@ -0,0 +1,101 @@
+"""
+This is the thing and...
+    it now works
+
+T̶h̶i̶s̶ ̶i̶s̶ ̶t̶h̶e̶ ̶t̶h̶i̶n̶g̶ ̶b̶u̶t̶.̶.̶.̶
+    \̶t̶-̶ ̶i̶t̶ ̶d̶o̶e̶s̶n̶t̶ ̶w̶o̶r̶k̶ ̶f̶o̶r̶ ̶s̶o̶m̶e̶ ̶r̶e̶a̶s̶o̶n̶
+"""
+
+from Resources.resources import SOLVED, BOARD
+from Resources.resources import display
+import numpy as np
+
+
+SOLVED = np.array(SOLVED)
+BOARD  = np.array(BOARD)
+
+
+options = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+def get_subpart(board, i, j):
+    if i in (0, 1, 2):
+        if   j in (0, 1, 2):
+            return board[0:3,0:3]
+        elif j in (3, 4, 5):
+            return board[0:3,3:6]
+        elif j in (6, 7, 8):
+            return board[0:3,6:9]
+
+    elif i in (3, 4, 5):
+        if   j in (0, 1, 2):
+            return board[3:6,0:3]
+        elif j in (3, 4, 5):
+            return board[3:6,3:6]
+        elif j in (6, 7, 8):
+            return board[3:6,6:9]
+
+    elif i in (6, 7, 8):
+        if   j in (0, 1, 2):
+            return board[6:9,0:3]
+        elif j in (3, 4, 5):
+            return board[6:9,3:6]
+        elif j in (6, 7, 8):
+            return board[6:9,6:9]
+
+
+def is_valid(board, i, j, option):
+    if option in board[i] or option in board[:,j]:
+        return False
+
+    subpart = get_subpart(board, i, j)                  # returns a 3x3 np array
+    part = [item for row in subpart for item in row]  # returns a list with all the elems of the 3x3 array above
+    if option in part:
+        return False
+
+    return True
+
+
+def solve(board, row, col) -> bool:
+    if board[row][col] == 0:
+
+        for option in options:
+            if is_valid(board, row, col, option):
+                board[row][col] = option
+
+                if 0 not in board:
+                    return True
+
+                if col == 8:
+                    if solve(board, row+1, 0):
+                        return True
+
+                else:
+                    if solve(board, row, col+1):
+                        return True
+
+        else:
+            board[row][col] = 0
+            return False
+     
+    elif col == 8:
+        # All I had to do was return this result...
+        # because otherwise the function returns None
+        # instead of the expected True (or False).
+        return solve(board, row+1, 0)
+    else:
+        return solve(board, row, col+1)
+
+    
+def main(board):
+    print(all(x.all()==y.all() for x,y in zip(board, SOLVED)))   # should print False
+    
+    display(board)
+    print(solve(board, 0, 0))
+    display(board)
+
+    print(all(x.all()==y.all() for x,y in zip(board, SOLVED)))   # should print True
+
+    
+if __name__ == '__main__':
+    main(BOARD)
